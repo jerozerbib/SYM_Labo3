34144f471bd389cad68220640df87b0906bf81c9
David Jaquet <djaquet5@gmail.com>
2019-11-15 10:55:34 +0100

    Template done

---
+++ code/app/src/main/java/ch/heig/labo3/MainActivity.java
@@ -0,0 +1,32 @@
+package ch.heig.labo3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button nfc;
+    private Button bluetooth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nfc = findViewById(R.id.nfc);
+        nfc.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, NFCActivity.class);
+            startActivityForResult(intent, 1);
+        });
+
+        bluetooth = findViewById(R.id.bluetooth);
+        bluetooth.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, BluetoothActivity.class);
+            startActivityForResult(intent, 1);
+        });
+
+    }
+}
---
+++ code/app/src/main/java/ch/heig/labo3/NFCActivity.java
@@ -0,0 +1,14 @@
+package ch.heig.labo3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NFCActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc);
+    }
+}
