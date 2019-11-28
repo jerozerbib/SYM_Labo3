44cf9d599f67f3c6fa03743ba6172bf84ddc3d49
David Jaquet <djaquet5@gmail.com>
2019-11-28 12:10:43 +0100

    bind scan variable with activity button

--- code/app/src/main/java/ch/heig/labo3/BarCodeActivity.java
+++ code/app/src/main/java/ch/heig/labo3/BarCodeActivity.java
@@ -3,12 +3,20 @@ package ch.heig.labo3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class BarCodeActivity extends AppCompatActivity {
+
+    private Button scan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bar_code);
+
+        scan = findViewById(R.id.scan);
+        scan.setOnClickListener(v -> {
+//            IntentIntegrator integrator = new IntentIntegrator(BarCodeActivity.this);
+        });
     }
 }
